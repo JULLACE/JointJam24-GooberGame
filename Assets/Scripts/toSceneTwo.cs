f589a901c144150af98a2eb4c5a2056704cdb2f8
Julio <88823380+JULLACE@users.noreply.github.com>
2024-03-31 16:12:48 -0400

    Last minute levels

--- Assets/Scripts/toSceneTwo.cs
+++ Assets/Scripts/toSceneTwo.cs
@@ -12,7 +12,6 @@ public class toSceneTwo : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
      if(other.CompareTag("Player")) {
         SceneManager.LoadScene(index);
-        //SceneManager.LoadScene(levelName);
     }
 }
 
